a28d7750e77b1c97154b5495ae1c40f568b96a86
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-05-05 04:20:33 +0530

    add comment

--- week00/variables/formatting_strings.go
+++ week00/variables/formatting_strings.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+/*
+Create a new variable called msg on line 9 and use the appropriate formatting function to return a string that contains following:
+
+Hi NAME, your open rate is OPENRATE percentNEWLINE
+
+1. Replace NAME with the variable name,
+2. Replace OPENRATE with the variable openRate rounded to the nearest "tenths" place, e.g 10.523 should be rounded down to 10.5
+3. The word percent should appear as part of the string following the open rate value
+4. Replace NEWLINE with the newline \n escape sequence.
+*/
+
 func main() {
 	const name = "Saul Goodman"
 	const openRate = 30.5
